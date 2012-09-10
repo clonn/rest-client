804f0353428e5c94023f130e5e2e17fa498b73c6
Caesar Chi <clonncd@gmail.com>
2012-09-10 10:55:50 +0000

    refactor index.js module.

--- index.js
+++ index.js
@@ -10,8 +10,11 @@
 
 var request = require("request");
 var url = require("url");
+var setEnvFlag = false;
 // String test for http url opening.
 var HTTP_REGEXP = /^http:\/\//;
+// default setting Timeout is 30 second.
+var TIMEOUT = 30 * 1000;
 // env require keys.
 var ENV_REQUIRE_KEYS = [
   "hostname",
@@ -47,10 +50,27 @@ var testHttpUrl = function (url) {
 };
 
 /**
+ * Set timeout, set time second
+ *
+ * @method setTimeout
+ * @params {Int} time
+ * @return {Int} time
+ */
+exports.setTimeout = function (time) {
+  time = parseInt(time, 10);
+  if (time < 1000) {
+    throw new Error("[Error] setTimeout, time second need more than 1000 or an integer");
+    return;
+  }
+
+  return TIMEOUT = time;
+};
+
+/**
  * Setter, set env, a host environment variable.
  *
  * @method setEnv
- * @params {Object} arg
+ * @params {Object} url object
  * @return {Object} env object
  */
 exports.setEnv = function (arg) {
@@ -58,11 +78,18 @@ exports.setEnv = function (arg) {
   // get env key as an array.
   var envKeys = Object.keys(ENV_REQUIRE_KEYS);
 
+  if (typeof arg !== "object") {
+    throw new Error("[ERROR] setEnv, params should an object");
+    return false;
+  }
+
   // parse url string to url object , using url object
   // convert url string to url Object.
   if (testHttpUrl(arg)) {
     arg = url.parse(arg);
   }
+
+  env = arg;
 
   // filter url object, it is suit on conviention.
   ENV_REQUIRE_KEYS.forEach(function (key, id) {
@@ -71,7 +98,7 @@ exports.setEnv = function (arg) {
     }
   });
 
-  env = arg;
+  setEnvFlag = true;
   return env;
 };
 
@@ -83,5 +110,18 @@ exports.setEnv = function (arg) {
  * @return {Object} env object
  */
 exports.send = function (arg, callback) {
+  console.log('run send');
 
+  var option = arg;
+
+
+  // set option to env
+  option.timeout = TIMEOUT;
+  option = setEnvFlag ? env : this.setEnv(option);
+
+  // Send request
+  request(arg, function (error, res, body) {
+    // TODO: need to warp it to easy use.
+    callback(error, res, body);
+  });
 };
