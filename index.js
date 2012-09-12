3b6b210b3c6fce44b8720f461d35a5f22796ac9a
Caesar Chi <caesar_chi@miiicasa.com>
2012-09-12 17:57:57 +0800

    update index.js, refactory a lot of part code to separate them.

--- index.js
+++ index.js
@@ -11,24 +11,28 @@
 var request = require("request"),
     util    = require('util'),
     url     = require("url"),
-    Emitter   = require('events').EventEmitter,
+    _       = require("underscore"),
+    Emitter  = require('events').EventEmitter,
     emitter = new Emitter(),
     // a fail event name
     EVENT_FAIL_NAME = 'onerror',
     // String test for http url opening.
-    HTTP_REGEXP = /^http:\/\//,
+    HTTP_REGEXP = /^http:\/\/|^https:\/\//,
     // set default url for env
-    defaultUrl = {
+    defaultURL = {
       protocol: 'http',
       host: 'api.micloud.tw:80',
       hostname: 'api.micloud.tw',
       port: 80
     },
+    // for a user URL object for next convert to env.url
+    userUrlBuffer = {},
     // env is service connect environment variable.
     env = {
+      protocol: defaultURL.protocol,
+      host: defaultURL.host,
       // url object
-      url: defaultUrl,
-      uri: url.format(defaultUrl),
+      url: url.format(defaultURL),
       // default use GET method
       method: 'GET',
       // default set 30 seconds
@@ -37,23 +41,91 @@ var request = require("request"),
     // ==================
     //  private method
     // ==================
-    testHttpUrl;
+    sendRequest,
+    composeURL,
+    isHttpURL;
+
+sendRequest = function (requestObj, callback) {
+  if (_.isObject(requestObj)) {
+
+    // show url
+    console.log("[STATUS] sendRequest, url: " + env.url);
+
+    var requestUrlObj = env;
+    requestUrlObj.protocol = undefined;
+
+    request(
+      requestUrlObj,
+      function (error, res, body) {
+
+        // send error message
+        if (error) {
+          return emitter.emit(EVENT_FAIL_NAME, new Error('[ERROR] can not connect to server, url: ' + env.url));
+        }
+
+        // normal send request
+        try {
+          var data = JSON.parse(body);
+        } catch (e) {
+          var data = {
+            status: 'ERROR',
+            message: 'Data convert to JSON fail'
+          };
+        }
+        callback(res, data);
+      }
+    );
+  }
+};
+
+
+
+composeURL = function (userURL) {
+  // set user url, and convert to domain name;
+  // re-parse url object
+  var urlObj;
+
+  if (_.isObject(userURL)) {
+
+    userUrlBuffer = _.extend(env, userURL);
+    env.url = url.format(userUrlBuffer);
+
+    return env.url;
+
+  }
+
+  if (isHttpURL(userURL)) {
+
+    urlObj  = url.parse(userURL);
+    userUrlBuffer = _.extend(env, urlObj);
+    env.url = userURL;
+
+  } else {
+
+    // URL Object original attribute
+    userUrlBuffer = _.extend(env, { pathname: userURL });
+    env.url = url.format(userUrlBuffer);
+
+  }
+
+  return env.url;
+
+};
 
 /**
  * Test url parttern by REGEXP.
  *
- * @method testHttpUrl
+ * @method isHttpURL
  * @params {String} url
  * @return {Boolean}
  */
-testHttpUrl = function (url) {
+isHttpURL = function (url) {
   // parse url string to url object , using url object
   // convert url string to url Object.
   if (typeof url === "string") {
 
     // test http url opening.
     if ( ! HTTP_REGEXP.test(url)) {
-      throw new Error("[ERROR] URL parttern is not match.");
       return false;
     }
 
@@ -62,23 +134,6 @@ testHttpUrl = function (url) {
 
   // url type is not a string
   return false;
-};
-
-/**
- * Set timeout, set time second
- *
- * @method setTimeout
- * @params {Int} time
- * @return {Int} time
- */
-exports.setTimeout = function (time) {
-  time = parseInt(time, 10);
-  if (time < 1000) {
-    throw new Error("[Error] setTimeout, time second need more than 1000 or an integer");
-    return;
-  }
-
-  return TIMEOUT = time;
 };
 
 /**
@@ -97,18 +152,16 @@ exports.set = function (arg) {
   }
 
   // set new attributes to env object
-  for (i in arg) {
-    env[i] = arg[i];
-  }
+  env = _.extend(env, arg);
 
   // Always set Timeout more than 0
-  env.timeout = ((env.timeout = parseInt(env.timeout, 10)) > 0) ? env.timeout : 30000;
-
-  // re-parse url object
-  if (env.url && typeof env.url === "object") {
-    env.uri = url.format(env.url);
+  if (arg.timeout) {
+    env.timeout = ((env.timeout = parseInt(env.timeout, 10)) > 0) ? env.timeout : 30000;
   }
 
+  composeURL(env.url);
+
+  console.log('[STATUS] restClient set url: ' + env.url);
   return env;
 };
 
@@ -119,24 +172,31 @@ exports.set = function (arg) {
  * @params {Object} arg
  * @return {Object} env object
  */
-exports.send = function (reqUrl, callback) {
+exports.send = function (reqURL, callback) {
 
-  // set option to env
-  option.timeout = TIMEOUT;
-  option = setEnvFlag ? env : this.setEnv(option);
+  // URL must be a object.
+  if (_.isObject(reqURL)) {
+    env = _.extend(env, reqURL);
+  }
 
-  // Send request
-  request(arg, function (error, res, body) {
-    // when error, fire on fail event
-    if (error) {
-      emitter.emit(EVENT_FAIL_NAME, error, res, body);
-    }
+  if (typeof reqURL === "string") {
+    env.url = reqURL;
+  }
 
-    // TODO: need to warp it to easy use.
-    callback(res, body);
-  });
+  // Check URL string, and suit on http portocal.
+  composeURL(env.url);
+
+  return sendRequest(env, callback);
+
 };
 
+/**
+ * Send data
+ *
+ * @method send
+ * @params {Object} arg
+ * @return {Object} env object
+ */
 exports.error = function (callback) {
   emitter.on(EVENT_FAIL_NAME, callback);
 };
